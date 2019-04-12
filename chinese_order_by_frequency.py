200311b5bdfdd554b95dfc85a3b2eedf054576a8
Max Allen <maxallen@mit.edu>
2019-04-13 01:16:00 +0800

    Can now parse highlighted vocab from html file

--- chinese_order_by_frequency.py
+++ chinese_order_by_frequency.py
@@ -1,14 +1,22 @@
-input_file = "input.txt"
+import re
+from bs4 import BeautifulSoup
+input_file = "input.html"
 frequency_files = [
     ("Resources/Frequency_Dictionaries/global_wordfreq.release_UTF-8.txt", '	')]
 MAX_RANK = 1000000  # Number larger than number of words in any frequency dictionary, used for sorting words not found in frequency dictionary
-
 words = []
-with open(input_file, 'r') as in_file:
-    for line in in_file:
-        words.append(line.strip())
 
-# print("Words in input list: \n{}".format(words))
+# Infile is html with highlighted words
+with open(input_file) as in_file:
+    soup = BeautifulSoup(in_file)
+    highlighted_spans = soup.find_all(
+        "span", style=re.compile("background-color"))
+    words = [span.string for span in highlighted_spans]
+
+# Infile is txt file list of vocab words
+# with open(input_file, 'r') as in_file:
+#     for line in in_file:
+#         words.append(line.strip)
 
 # Order words list by frequency according to each dict
 # Words not found in frequency dictionary are put last
