9a2bd9dbc7dc823c68a3e0c12f2192ac2e128620
Max Allen <maxallen@mit.edu>
2019-04-13 14:27:27 +0800

    Now parses html files, and outputs html file with word list table

--- chinese_order_by_frequency.py
+++ chinese_order_by_frequency.py
@@ -1,14 +1,20 @@
 import re
 from bs4 import BeautifulSoup
-input_file = "input.html"
+import pinyin
+import pinyin.cedict
+
+input_file = "Word_List_Files/996.html"
+output_file = "Word_List_Files/996_Wordlist.html"
 frequency_files = [
     ("Resources/Frequency_Dictionaries/global_wordfreq.release_UTF-8.txt", '	')]
 MAX_RANK = 1000000  # Number larger than number of words in any frequency dictionary, used for sorting words not found in frequency dictionary
 words = []
 
+# ---------------- Parse File To Get Word List ---------------- #
+
 # Infile is html with highlighted words
 with open(input_file) as in_file:
-    soup = BeautifulSoup(in_file)
+    soup = BeautifulSoup(in_file, "lxml")
     highlighted_spans = soup.find_all(
         "span", style=re.compile("background-color"))
     words = [span.string for span in highlighted_spans]
@@ -18,6 +24,7 @@ with open(input_file) as in_file:
 #     for line in in_file:
 #         words.append(line.strip)
 
+# ---------------- Create Frequency List Dictionary ---------------- #
 # Order words list by frequency according to each dict
 # Words not found in frequency dictionary are put last
 for freq_file in frequency_files:
@@ -30,8 +37,43 @@ for freq_file in frequency_files:
             freq_dict[char] = rank
             rank += 1
 
+# ---------------- Order Words By Frequency and Export ---------------- #
+
     word_copy = [(word, freq_dict.get(word, MAX_RANK)) for word in words]
     word_copy.sort(key=lambda x: x[1])
 
     print("List sorted by {}".format(freq_file[0]))
     print(word_copy)
+
+html_doc = ""
+with open('Word_List_Files/wordlist_header.html', 'r') as header_file:
+    html_doc += header_file.read()
+
+for word in words:
+    chinese_word = """<td class="c8" colspan="1" rowspan="1"><p class="c2"><span class="c11">""" + \
+        word + """</span></p></td>"""
+    pinyin_word = """<td class="c7" colspan="1" rowspan="1">
+    <p class="c1">
+      <span class="c0">""" + pinyin.get(word) + """</span>
+    </p>
+  </td>"""
+    translation = pinyin.cedict.translate_word(word) or []
+    meaning = """<td class="c5" colspan="1" rowspan="1">
+    <p class="c1">
+      <span class="c0">""" + "; ".join(translation) + """</span>
+    </p>
+  </td>"""
+    use = """<td class="c3" colspan="1" rowspan="1">
+    <p class="c1">
+      <span class="c0"></span>
+    </p>
+  </td>"""
+    row_string = """<tr class="c6">""" + chinese_word + \
+        pinyin_word + meaning + use + """</tr>"""
+    html_doc += row_string
+
+with open('Word_List_Files/wordlist_footer.html', 'r') as footer_file:
+    html_doc += footer_file.read()
+
+with open(output_file, 'w') as out_file:
+    out_file.write(html_doc)
